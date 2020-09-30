451b37bc7b7cd7694e4ff9b09d7e27043caab6db
leondante-ctrl <maxgamerdu@gmail.com>
2020-10-01 01:04:31 +0300

    made necessary imports as well as created user class

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from flask_login import UserMixin
+from datetime import datetime
+from . import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+    workouts = db.relationship('Workout', backref='author', lazy=True)
