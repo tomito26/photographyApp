213c7c937b9d3a04b263148a952965eb4a8812b7
tomito26 <tommybwah@gmail.com>
2020-10-01 21:29:19 +0300

    added authentication system

--- config.py
+++ config.py
@@ -4,7 +4,12 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:tom@localhost/photographyweb'
-
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
@@ -28,7 +33,7 @@ class DevConfig(Config):
     '''
 
     DEBUG = True
-    
+
 config_options = {
     'development':DevConfig,
     'production':ProdConfig,
--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+export SECRET_KEY ='wedse657l'
+export MAIL_USERNAME=  
+export MAIL_PASSWORD=
 python3.8 manage.py server
