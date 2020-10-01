4768462098317f311406e01ebfde0f7c9331464b
rickymwethera <rickymwethera@gmail.com>
2020-10-01 16:35:35 +0300

    added models to be pulled

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Categories:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+
+class Images:
+    def __init__(self,id,image,date,title):
+        self.id = id
+        self.image = image
+        self.date = date
+        self.title = title
