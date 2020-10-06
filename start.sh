d2817261d56132a24413dbe54e9c6607ded36904
Thomas Khaemba <tommybwah@gmail.com>
2020-10-06 03:28:10 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-export IMAGE_API_KEY='18539910-56b2544ee9e5c78d29fe4e306'
-export SECRET_KEY='wedse657l17ndcsuod'
-export MAIL_USERNAME='tommybwah@gmail.com'
-export MAIL_PASSWORD='thom.66as'
-export DATABASE_URL='postgresql+psycopg2://moringa:tom@localhost/photographyweb'
-
-python3.8 manage.py server
